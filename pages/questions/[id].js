022d11feae891df12e23bafcac0f176db8cf12ee
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2022-05-08 01:06:03 -0700

    Add question page

--- pages/questions/[id].js
+++ pages/questions/[id].js
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { doc } from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
 import { questionsCollection } from "../../firebaseConfig";
 
 export default function QuestionPage() {
@@ -13,7 +13,7 @@ export default function QuestionPage() {
       return;
     }
     const questionRef = doc(questionsCollection, id);
-    const question = await questionRef.get();
+    const question = await getDoc(questionRef);
     setQuestion(question.data());
   }
 
@@ -22,8 +22,26 @@ export default function QuestionPage() {
   }, [id]);
 
   return <>
-    <div>QuestionPage</div>
-    <h2>{question?.title}</h2>
-    <p>{question?.body}</p>
+    <div className="py-10">
+      <header>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <span
+            className="mt-2 block text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+              Question:
+            <span className="ml-3 font-normal">
+              {question?.title}
+            </span>
+          </span>
+        </div>
+      </header>
+      <main>
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <p className="mt-6 text-xl text-gray-500 leading-8">
+            {question?.body}
+          </p>
+        </div>
+      </main>
+    </div>
   </>;
+  ;
 }
