fe09d2fa408f7118af7eac0d01c74d561a7ef569
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2022-05-08 06:29:38 -0700

    Fix firebase functions issues

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require("firebase-functions");
 
+require("dotenv").config();
+
 const admin = require("firebase-admin");
 const MonkeyLearn = require("monkeylearn");
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
@@ -22,23 +24,21 @@ exports.sendTexts = functions.firestore
     const { title, body, bounty } = snapshot.data();
     const data = [body];
     const res = await ml.extractors.extract(modelId, data);
-    let topics = res.body[0].extractions;
-    topics.sort(function(a, b) {
+    let skills = res.body[0].extractions;
+    skills.sort(function(a, b) {
       return b.relevance - a.relevance;
     });
-    topics = topics.slice(0, 10);
+    skills = skills.slice(0, 10);
     const usersRef = admin.firestore().collection("users");
-    const queryRef = usersRef.where("topics", "array-contains-any", topics)
+    const queryRef = usersRef.where("skills", "array-contains-any", skills)
       .where("bounty", "<=", bounty);
     const users = await queryRef.get();
-    const usersPhoneNums = users.docs.map(doc => doc.data().phoneNum);
+    const usersPhoneNums = users.docs.map(doc => doc.data().phoneNum).filter(Boolean);
     const message = `A new question, "${title}", that you might be an expert on
-    has been posted for $${bounty}. Check it out!`;
+    has been posted for $${bounty}. Check it out: https://dereference.tech/questions/${snapshot.ref.id}`;
     const promises = usersPhoneNums.map(phoneNum => {
       return client.messages.create({
-        body: message,
-        from: "+19705389873",
-        to: phoneNum,
+        body: message, from: "+19705389873", to: phoneNum,
       });
     });
     await Promise.all(promises);
